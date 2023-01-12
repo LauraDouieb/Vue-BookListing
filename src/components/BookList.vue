19f45ce070adcd93a839d626d6105b87daa789c3
Laura <laura.stievenard@capgemini.com>
2023-01-12 10:16:24 +0100

    Correction liste de livres

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -13,11 +13,11 @@
         data() {
             return {
                 title : "All Books",
-                book : [
+                books : [
                     {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
                     {title: 'American Gods', author: 'Neil Gaiman'},
                     {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-                ],
+                ]
             };
         },
     };
