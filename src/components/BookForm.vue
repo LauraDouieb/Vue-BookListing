13fea087b0351ca18eba4f3b69a4cffba648b228
Laura <laura.stievenard@capgemini.com>
2023-01-13 14:58:12 +0100

    BookForm V1

---
+++ src/components/BookForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form class="" action="#" method="post">
+        <input type="text" name="title" value="" placeholder="Book Title">
+        <input type="text" name="author" value="" placeholder="Book Author">
+        <button type="submit" name="button" v-on:click="bookSubmit">Add Book</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name : 'BookForm',
+        props: ['book'],
+        data() {
+            return {
+                bookTitle : '',
+                bookAuthor : ''
+            };
+        },
+    }
+    method : {
+        bookSubmit () {
+            
+        }
+    }
+</script>
+
+<style>
+
+</style>
